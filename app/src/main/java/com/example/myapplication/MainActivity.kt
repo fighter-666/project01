b936559729ed867782c7c51616c1f64952b66536
wudm <977657115@qq.com>
2023-07-16 21:57:38 +0800

    tablayout  icon优化

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,10 +28,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val tabs = arrayOf("    Components", "         Helper", "       Lab")
-        val image1 = R.drawable.ic_canyin
-        val image2 = R.drawable.bgs
-        val image3 = R.drawable.images
+        val tabs = arrayOf("  Components", " Helper", "Lab")
+        val image1 = R.mipmap.icon_tabbar_component_selected
+        val image2 = R.mipmap.icon_tabbar_util_selected
+        val image3 = R.mipmap.icon_tabbar_lab_selected
         val pics = arrayOf(image1,image2,image3)
         binding.viewPager.offscreenPageLimit = ViewPager2.OFFSCREEN_PAGE_LIMIT_DEFAULT
         binding.viewPager.adapter = object : FragmentStateAdapter(supportFragmentManager,lifecycle) {
@@ -52,38 +52,12 @@ class MainActivity : AppCompatActivity() {
         
         val mediator = TabLayoutMediator(binding.tabLayout,binding.viewPager) { tab,position ->
             val tabView = LayoutInflater.from(this@MainActivity).inflate(R.layout.custom_tab_view, null)
-            //val tabView = TextView(this@MainActivity)
-            val states = arrayOf(
-                intArrayOf(android.R.attr.state_selected),
-                intArrayOf()
-            )
-//            for (index in 0..binding.tabLayout.tabCount) {
-//                binding.tabLayout.getTabAt(index)?.setIcon(R.drawable.bgs)
-//            }
-//            binding.tabLayout.getTabAt(0)?.setIcon(pics[0])
-//            binding.tabLayout.getTabAt(1)?.setIcon(pics[1]);
-//            binding.tabLayout.getTabAt(2)?.setIcon(pics[2]);
-            val colors = intArrayOf(activeColor, normalColor)
-            val colorStateList = ColorStateList(states, colors)
-//            tabView.text = tabs[position]
-//            tabView.setTextSize(normalSize.toFloat())
-//            tabView.setTextColor(colorStateList)
-
             val tabIcon = tabView.findViewById<ImageView>(R.id.tab_icon)
             val tabTitle = tabView.findViewById<TextView>(R.id.tab_title)
-
             tabTitle.text = tabs[position]
             tabIcon.setImageResource(pics[position])
             tab.customView = tabView
             tab.setIcon(pics[position])
-            // 设置选项卡的图标
-//            when (position) {
-//                0 -> tab.setIcon(pics[0])
-//                1 -> tab.setIcon(pics[1])
-//                2 -> tab.setIcon(pics[2])
-//                else -> {} // 处理其他位置的选项卡，如果有的话
-//            }
-
         }
         mediator.attach()
     }
